1663dcdb36c00daabbf0b2779935e147bc48dc82
mah <mahsa.79mirmiran@gmail.com>
2024-02-25 00:42:40 +0300

    bouquest made +responisve

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import HeaderLanding from '../lib/components/Header-landing.svelte';
+	import Bouquest from '../lib/components/bouquest.svelte';
 </script>
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class=" h-full w-full mx-auto flex justify-center items-center bg-white">
+<div class=" h-full w-full mx-auto flex flex-col justify-center items-center bg-white">
 	<HeaderLanding />
+	<Bouquest />
 </div>
